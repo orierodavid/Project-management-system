0e547961dd5d52bcac3a7f5a0ee104c9a022a2fb
orierodavid <orierodavid@yahoo.com>
2026-09-04 19:48:17 +0100

    Expose attendance status endpoint

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AttendanceController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
+    Route::get('/attendance/status', [AttendanceController::class, 'status'])->name('attendance.status');
     Route::post('/attendance/clock-in', [AttendanceController::class, 'clockIn'])->name('attendance.clock-in');
     Route::post('/attendance/clock-out', [AttendanceController::class, 'clockOut'])->name('attendance.clock-out');
 });
