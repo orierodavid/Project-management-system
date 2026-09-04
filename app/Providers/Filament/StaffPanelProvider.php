cc5adb38ede831a123aad68da373169fc3234c76
orierodavid <orierodavid@yahoo.com>
2026-09-04 19:49:14 +0100

    Enable staff dashboard widgets

--- app/Providers/Filament/StaffPanelProvider.php
+++ app/Providers/Filament/StaffPanelProvider.php
@@ -28,6 +28,7 @@ class StaffPanelProvider extends PanelProvider
             ->colors(['primary' => Color::Blue])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
+            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->pages([Dashboard::class])
             ->middleware([
                 EncryptCookies::class,
