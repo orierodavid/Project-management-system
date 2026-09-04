6023ff923e74aeb5eb1702a57f8ccd7928001606
orierodavid <orierodavid@yahoo.com>
2026-09-04 19:39:19 +0100

    feat: add branch edit page

---
+++ app/Filament/Resources/BranchResource/Pages/EditBranch.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\BranchResource\Pages;
+
+use App\Filament\Resources\BranchResource;
+use Filament\Resources\Pages\EditRecord;
+
+class EditBranch extends EditRecord
+{
+    protected static string $resource = BranchResource::class;
+}
