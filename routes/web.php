118aa68e87e814d8a635af531e255f8a9fa7927b
orierodavid <orierodavid@yahoo.com>
2026-09-04 21:23:26 +0100

    feat: expose browser installer route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\AttendanceController;
+use App\Http\Controllers\InstallerController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/install', [InstallerController::class, 'index'])->name('install');
+Route::post('/install', [InstallerController::class, 'install'])->name('install.run');
 
 Route::middleware('auth')->group(function () {
     Route::get('/attendance/status', [AttendanceController::class, 'status'])->name('attendance.status');
