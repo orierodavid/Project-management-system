2d697fe79b0bf593b556a086e1e3a357674d4fce
orierodavid <orierodavid@yahoo.com>
2026-09-04 19:40:32 +0100

    build: add application service provider

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        // Application service bindings belong here.
+    }
+
+    public function boot(): void
+    {
+        // Application bootstrapping belongs here.
+    }
+}
