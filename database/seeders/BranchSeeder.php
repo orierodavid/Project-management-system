d74f4165c54059f6d730a8d77c5aba865d94473f
orierodavid <orierodavid@yahoo.com>
2026-09-04 19:35:43 +0100

    feat: add default branch seeder

---
+++ database/seeders/BranchSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Branch;
+use Illuminate\Database\Seeder;
+
+class BranchSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Branch::firstOrCreate(
+            ['name' => 'Head Office'],
+            [
+                'address' => 'Configure your company head office address',
+                'latitude' => 0,
+                'longitude' => 0,
+                'radius_meters' => 100,
+                'is_active' => true,
+            ],
+        );
+    }
+}
