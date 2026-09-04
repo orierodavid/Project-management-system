fa1ee0f75204aa4e96741e9082d4886434a805f8
orierodavid <orierodavid@yahoo.com>
2026-09-04 19:40:04 +0100

    feat: add task listing page

---
+++ app/Filament/Resources/TaskResource/Pages/ListTasks.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Filament\Resources\TaskResource\Pages;
+
+use App\Filament\Resources\TaskResource;
+use Filament\Actions\CreateAction;
+use Filament\Resources\Pages\ListRecords;
+
+class ListTasks extends ListRecords
+{
+    protected static string $resource = TaskResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return auth()->user()?->can('manage-tasks') ? [CreateAction::make()] : [];
+    }
+}
