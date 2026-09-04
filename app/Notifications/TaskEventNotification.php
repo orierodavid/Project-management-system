f6d3838c85cb1b2d64a2875138899eefe91c4742
orierodavid <orierodavid@yahoo.com>
2026-09-04 20:15:45 +0100

    Queue task event notifications

--- app/Notifications/TaskEventNotification.php
+++ app/Notifications/TaskEventNotification.php
@@ -4,10 +4,11 @@ namespace App\Notifications;
 
 use App\Models\Task;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class TaskEventNotification extends Notification
+class TaskEventNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
