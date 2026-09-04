2cd3152323319a9c326d540af02a8d0abae912ec
orierodavid <orierodavid@yahoo.com>
2026-09-04 19:40:08 +0100

    feat: add task creation attribution

---
+++ app/Filament/Resources/TaskResource/Pages/CreateTask.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Filament\Resources\TaskResource\Pages;
+
+use App\Filament\Resources\TaskResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateTask extends CreateRecord
+{
+    protected static string $resource = TaskResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['assigned_by'] = auth()->id();
+        return $data;
+    }
+}
