2117d022d6997f498b821b5ae92de86ae1cafd0c
orierodavid <orierodavid@yahoo.com>
2026-09-04 19:42:15 +0100

    Configure activity logging

---
+++ config/activitylog.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'enabled' => env('ACTIVITY_LOGGER_ENABLED', true),
+    'delete_records_older_than_days' => 365,
+    'default_log_name' => 'default',
+    'database_connection' => env('DB_CONNECTION', 'mysql'),
+    'table_name' => 'activity_log',
+    'activity_model' => Spatie\Activitylog\Models\Activity::class,
+    'subject_returns_soft_deleted_models' => false,
+    'causer_returns_soft_deleted_models' => false,
+    'changes' => [
+        'enabled' => true,
+        'attribute_getters' => false,
+        'attribute_setters' => false,
+    ],
+    'submit_empty_logs' => false,
+    'activity' => [
+        'batch_uuid' => true,
+    ],
+];
