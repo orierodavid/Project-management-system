76c4d072dcc9040694d9c8f4c8745788a04949da
orierodavid <orierodavid@yahoo.com>
2026-09-04 19:39:24 +0100

    feat: add department resource

---
+++ app/Filament/Resources/DepartmentResource.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Models\Department;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class DepartmentResource extends Resource
+{
+    protected static ?string $model = Department::class;
+    protected static ?string $navigationIcon = 'heroicon-o-users';
+    protected static ?string $navigationGroup = 'Organization';
+
+    public static function canViewAny(): bool
+    {
+        return auth()->user()?->can('manage-departments') ?? false;
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form->schema([
+            TextInput::make('name')->required()->maxLength(255),
+            Textarea::make('description')->rows(4),
+            Toggle::make('is_active')->default(true),
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table->columns([
+            TextColumn::make('name')->searchable()->sortable(),
+            TextColumn::make('users_count')->counts('users')->label('Staff'),
+            IconColumn::make('is_active')->boolean(),
+            TextColumn::make('created_at')->dateTime()->sortable(),
+        ])->defaultSort('name');
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListDepartments::route('/'),
+            'create' => Pages\CreateDepartment::route('/create'),
+            'edit' => Pages\EditDepartment::route('/{record}/edit'),
+        ];
+    }
+}
