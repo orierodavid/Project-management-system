3d0e49719332bd498cde0bc5ba51f919ff52a1af
orierodavid <orierodavid@yahoo.com>
2026-09-04 19:42:46 +0100

    Add session configuration

---
+++ config/session.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'driver' => env('SESSION_DRIVER', 'database'),
+    'lifetime' => (int) env('SESSION_LIFETIME', 120),
+    'expire_on_close' => env('SESSION_EXPIRE_ON_CLOSE', false),
+    'encrypt' => env('SESSION_ENCRYPT', false),
+    'files' => storage_path('framework/sessions'),
+    'connection' => env('SESSION_CONNECTION'),
+    'table' => env('SESSION_TABLE', 'sessions'),
+    'store' => env('SESSION_STORE'),
+    'lottery' => [2, 100],
+    'cookie' => env('SESSION_COOKIE', 'project_management_session'),
+    'path' => '/',
+    'domain' => env('SESSION_DOMAIN'),
+    'secure' => env('SESSION_SECURE_COOKIE'),
+    'http_only' => true,
+    'same_site' => 'lax',
+];
