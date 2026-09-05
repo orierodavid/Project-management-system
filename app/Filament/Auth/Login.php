7f5326177c75047ca005da3a7345d9d9dc9ca059
orierodavid <orierodavid@yahoo.com>
2026-09-05 23:52:35 +0100

    Fix Pint formatting in admin login

--- app/Filament/Auth/Login.php
+++ app/Filament/Auth/Login.php
@@ -2,13 +2,14 @@
 
 namespace App\Filament\Auth;
 
+use Filament\Http\Responses\Auth\Contracts\LoginResponse;
 use Filament\Pages\Auth\Login as BaseLogin;
 
 class Login extends BaseLogin
 {
     protected static string $view = 'filament.auth.login';
 
-    public function authenticate(): ?\Filament\Http\Responses\Auth\Contracts\LoginResponse
+    public function authenticate(): ?LoginResponse
     {
         session()->forget('url.intended');
 
