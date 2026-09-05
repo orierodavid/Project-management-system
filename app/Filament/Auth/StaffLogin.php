3299b661897d5d10a235db800036631265a4cbef
orierodavid <orierodavid@yahoo.com>
2026-09-05 23:50:57 +0100

    Fix staff login redirect to panel dashboard

--- app/Filament/Auth/StaffLogin.php
+++ app/Filament/Auth/StaffLogin.php
@@ -7,4 +7,11 @@ use Filament\Pages\Auth\Login as BaseLogin;
 class StaffLogin extends BaseLogin
 {
     protected static string $view = 'filament.auth.staff-login';
+
+    public function authenticate(): ?\Filament\Http\Responses\Auth\Contracts\LoginResponse
+    {
+        session()->forget('url.intended');
+
+        return parent::authenticate();
+    }
 }
