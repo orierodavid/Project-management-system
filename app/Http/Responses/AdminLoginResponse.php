1aeec40ec9e01b42e20e0c539aaa240e138ba39d
orierodavid <orierodavid@yahoo.com>
2026-09-06 00:23:24 +0100

    Force Super Admin login to Admin dashboard

---
+++ app/Http/Responses/AdminLoginResponse.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Responses;
+
+use Filament\Http\Responses\Auth\Contracts\LoginResponse as LoginResponseContract;
+use Illuminate\Http\RedirectResponse;
+use Livewire\Features\SupportRedirects\Redirector;
+
+class AdminLoginResponse implements LoginResponseContract
+{
+    public function toResponse($request): RedirectResponse|Redirector
+    {
+        session()->forget('url.intended');
+
+        return redirect()->to('/admin/dashboard');
+    }
+}
