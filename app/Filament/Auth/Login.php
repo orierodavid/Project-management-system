b49d7865f69e41a94d763be979e3a445261a0bbf
orierodavid <orierodavid@yahoo.com>
2026-09-05 23:34:12 +0100

    Fix admin login redirect isolation

--- app/Filament/Auth/Login.php
+++ app/Filament/Auth/Login.php
@@ -7,4 +7,11 @@ use Filament\Pages\Auth\Login as BaseLogin;
 class Login extends BaseLogin
 {
     protected static string $view = 'filament.auth.login';
+
+    public function authenticate(): ?\Filament\Http\Responses\Auth\Contracts\LoginResponse
+    {
+        session()->forget('url.intended');
+
+        return parent::authenticate();
+    }
 }
