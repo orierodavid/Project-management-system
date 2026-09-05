ced930d7b5fa802e8d05e9e50d89337e299070a2
orierodavid <orierodavid@yahoo.com>
2026-09-05 23:58:12 +0100

    Fix StaffLogin Pint formatting

--- app/Filament/Auth/StaffLogin.php
+++ app/Filament/Auth/StaffLogin.php
@@ -2,14 +2,13 @@
 
 namespace App\Filament\Auth;
 
-use Filament\Http\Responses\Auth\Contracts\LoginResponse;
 use Filament\Pages\Auth\Login as BaseLogin;
 
 class StaffLogin extends BaseLogin
 {
     protected static string $view = 'filament.auth.staff-login';
 
-    public function authenticate(): ?LoginResponse
+    public function authenticate(): ?\Filament\Http\Responses\Auth\Contracts\LoginResponse
     {
         session()->forget('url.intended');
 
